ae8be77db9577c2812aeb28304fb5d3160fea183
Sebastian <86487434+Kuluzzi@users.noreply.github.com>
2023-01-09 12:17:00 +0100

    fileupload

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,28 @@
 // don't render the views in the backend-files!
-// 
-
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser') 
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const fs = require('fs')
 require('dotenv').config()
+
+const multer = require('multer')
+const upload = multer()
 
 const app = express()
 
-// access to the fs and the json-file in this case
-const fs = require('fs')
-const entriesData = require('./entries.json');
-const alltoken = require('./p.json');
-const { title, exit, nextTick, ppid } = require('process');
-const { parse } = require('path');
+const entriesData = require('./entries.json')
+const alltoken = require('./p.json')
 
-console.log('General setup');
+const { title, exit, nextTick, ppid } = require('process')
+const { parse } = require('path')
+
+console.log('General setup')
 app.use(cors())
 
 app.use(express.json())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 console.log('Adding routes')
 app.get('/api-status', (req, res) => {
@@ -28,18 +30,69 @@ app.get('/api-status', (req, res) => {
     res.send('If you see this, express server is working fine :)')
 })
 
+// protected route
+// jwt-token-name to check author.name
+// Check to make sure header is not undefined, if so, return Forbidden (403)
+function verifyJWT(req, res, next) {
+	const token = req.headers['authorization'];
+	if (!token) {
+	  return res.status(401).send({ message: 'No token provided.' });
+	}
+	jwt.verify(token, process.env.SECRET, (err, decoded) => {
+	  if (err) {
+		return res.status(401).send({ message: 'Invalid token.' });
+	  }
+	  req.user = decoded.user;
+	  next();
+	});
+  }
+
+app.post('/login/verify', (req, res) => {
+    const token = req.headers['authorization']
+    if (!token) return res.sendStatus(400)
+    jwt.verify(token, process.env.secretValueFromENV, (err, decoded) => {
+        if (err) {
+            console.log(`Error: `, err)
+            return res.sendStatus(403)
+        }
+        console.log(`decoded: `, decoded)
+        return res.send(decoded)
+    })
+})
+
+app.post('/login', (req, res) => {
+	// check if req.body.password is in the p.json-file
+	//const password = alltoken.praxis.find(password => alltoken.praxis.password = req.body.password)
+	const password = req.body.password
+	console.log(`sent password: `, password)
+    for (let i = 0; i < alltoken.praxis.length; i++) {
+        if (password == alltoken.praxis[i].password) {
+			console.log(`p.json password: `, alltoken.praxis[i].password)
+            // send the token to the frontend
+            const objectToSign = {
+			//	password: alltoken.praxis[i].password,
+                id: alltoken.praxis[i].id,
+                name: alltoken.praxis[i].authorName
+            }
+            const token = jwt.sign(objectToSign, process.env.secretValueFromENV)
+			console.log(`token: `, token)
+			console.log('/login-route triggered!')
+			// send the token with property token to the frontend
+            return res.send({ token })
+		}
+	}
+	res.send('Incorrect password')
+})
+
 app.get('/posts', (req, res) => {
     console.log("'/posts'-route triggered! ")
-    // console.log(entriesData.posts)
-
     // send posts to frontend for displaying them                                                                                    
     res.send(entriesData.posts)  
 })
-
+// get a specific post
 app.get('/posts/:id', (req, res) => {
     const id = req.params.id
     const allPosts = entriesData.posts
-    //const post = entriesData.posts.find(post => req.params.id)
     // a for loop that finds one post
     let reqPost = ""
         for(let post of allPosts) {
@@ -50,10 +103,19 @@ app.get('/posts/:id', (req, res) => {
         }
         console.log(`reqPost: `, reqPost)
         res.send(reqPost)
-    //res.json({ data: entriesData.posts.find((post) => post.id === req.params.id) })
-    console.log("'/posts/:id'-route triggered!")
+		console.log("'/posts/:id'-route triggered!")
 })
-
+// get a specific post
+/* app.get('/posts/:id', (req, res) => {
+	const post = entriesData.posts.find((entry) => entry.id === req.params.id)
+	if(post) {
+		res.send({ post })
+	} else {
+		res.status(404).send({ error: 'Post not found' })
+	}
+    console.log("'/posts/:id'-route triggered!")
+})*/
+// Get comments for a specific post
 app.get('/comments/:postId', (req, res) => {
     const pid = req.params.postId
     const allComments = entriesData.comments
@@ -70,170 +132,8 @@ app.get('/comments/:postId', (req, res) => {
         console.log("'/comments/:postId'-route triggered!")
 })
 
-app.post('/login/verify', (req, res) => {
-    const token = req.body.token
-    if (!token) return res.sendStatus(400)
-    jwt.verify(token, secretValueFromENV, (err, decoded) => {
-        if (err) {
-            console.log(`Error: `, err)
-            return res.sendStatus(403)
-        }
-        console.log(`decoded: `, decoded)
-        return res.send(decoded)
-    })
-})
-
-app.post('/login', (req, res) => {
-    const password = req.body.password
-    for (let i = 0; i < alltoken.praxis.length; i++) {
-        if (password == alltoken.praxis[i].password) {
-            // send the token to the frontend
-            const objectToSign = {
-                id: alltoken.praxis[i].id,
-                name: alltoken.praxis[i].praxisName
-            }
-            const token = jwt.sign(objectToSign, secretValueFromENV)
-            return res.send({ token })
-        }
-    }
-    res.send('Pass is incorrect!')
-})
-// middleware to be able to just write a post when logged in
-// write a route for logging in
-app.post('/login', (req, res) => {
-	// receive the login-data from the frontend
-	const loginData = req.body
-	console.log(`loginData: `, loginData)
-	// check if json-file not exists
-	if (!fs.existsSync('./p.json')) {
-		// create new file if not exists
-		fs.closeSync(fs.openSync('./p.json', 'w'))
-	}
-		//check if password is valid with alltoken file
-		if (loginData.password == alltoken.password) {
-			// if password is valid, send a token to the frontend
-			res.send
-			({
-				token:
-				{
-					//name: loginData.name,
-					password: loginData.password
-				}
-			})
-		}
-		else {
-			// if password is not valid, send a message to the frontend
-			res.send
-			({
-				message: "Wrong password"
-			})
-		}
-})
-
-// middleware to be able to just write a post when logged in
-// check if the token is valid
-const checkToken = (req, res, next) => {
-	// get the token from the frontend
-	const token = req.body.token
-	console.log(`token: `, token)
-	// check if the token is valid
-	if (token == alltoken.password) {
-		// if token is valid, go to the next middleware
-		next()
-	}
-	else {
-		// if token is not valid, send a message to the frontend
-		res.send
-		({
-			message: "Wrong token"
-		})
-	}
-}
-
-//Check to make sure header is not undefined, if so, return Forbidden (403)
-const checkJwt = (req, res, next) => {
-    const header = req.headers['authorization'];
-
-    if(typeof header !== 'undefined') {
-        const bearer = header.split(' ');
-        const token = bearer[1];
-        req.token = token;
-        next();
-    } else {
-        //If header is undefined return Forbidden (403)
-        res.sendStatus(403)
-    }
-}
-
-// write a route for writing a post
-app.post('/writePost', checkToken, (req, res) => {
-	// receive the post-data from the frontend
-	const postData = req.body
-	console.log(`postData: `, postData)
-	// check if json-file not exists
-	if (!fs.existsSync('./entries.json')) {
-		// create new file if not exists
-		fs.closeSync(fs.openSync('./entries.json', 'w'))
-	}
-	// read the json-file
-	const data = fs.readFileSync('./entries.json')
-	// parse the json-file
-	const json = JSON.parse(data)
-	// add the new post to the json-file
-	json.posts.push(postData)
-	// write the json-file
-	fs.writeFileSync('./entries.json', JSON.stringify(json))
-	// send a message to the frontend
-	res.send
-	({
-		message: "Post added"
-	})
-})
-
-/*
-// check for the sent password in the json-file
-app.post('/post', (req, res, next) => {
-	const { body } = req;
-	const { password } = body;
-
-	//checking to make sure the user entered the correct password combo
-	if(password === user.password) { 
-		//if user log in success, generate a JWT token for the user with a secret key
-		jwt.sign({user}, 'privatekey', { },(err, token) => {
-			if(err) { console.log(err) }    
-			res.send(token);
-		});
-	} else {
-		console.log('ERROR: Could not log in');
-	}
-})*/
-
-// protected route
-// jwt-token-name to check author.name
-
-app.get('/posts', checkJwt, (req, res) => {
-	//verify the JWT token generated for the user
-	jwt.verify(req.token, 'privatekey', (err, authorizedData) => {
-		if(err){
-			//If error send Forbidden (403)
-			console.log('ERROR: Could not connect to the protected route');
-			res.sendStatus(403);
-		} else {
-			//If token is successfully verified, we can send the autorized data 
-			res.json({
-				message: 'Successful log in',
-				authorizedData
-			});
-			console.log('SUCCESS: Connected to protected route');
-		}
-	})
-});
-
-// jwt-token-password to check author.password
-
-// write a route for adding a post
-
-app.post('/post', (req, res) => {
+app.post('/posts', (req, res) => {
+	const author = req.body.author // for the middeleware function "verifyJWT"
 	// receive the new post from the frontend
 	const newPost = req.body
 	console.log(`newPost: `, newPost)
@@ -258,10 +158,11 @@ app.post('/post', (req, res) => {
 		
 	// send the new post to the frontend
 	console.log("'/post'-route triggered!")
-    res.sendStatus(200, `Post added by ${newPost.author}`)
+    return res.send({ post: newPost })
 })
-// write a route for adding a comment for a post
-app.post('/comment', (req, res) => {
+
+// add a comment for a post
+app.post('/comments', (req, res) => {
 	// receive the new comment from the frontend
 	const newComment = req.body
 
@@ -290,33 +191,75 @@ app.post('/comment', (req, res) => {
 		fs.writeFileSync('./entries.json', JSON.stringify(entriesData))
 		
 	// send the new comment to the frontend
-	console.log("'/comment'-route triggered!")
-    res.sendStatus(200, `Comment added for postID ${newComment.postId}.`)
+	console.log("'/comments'-route triggered!")
+    res.send(newComment)
 })
-app.get('/login', (req, res) => {
-	console.log("'/login' get-route triggered!")
-	res.send(`Login page`)
-})
-app.post('/login', (req, res) => {
-	// check if the password sent from the frontend is similar to the password in the p.json file
-	const password = req.body.password
-	console.log(`/login post-route triggered!`)
-	// iterate through the p.json file
-	for(let i = 0; i < alltoken.length; i++){
-		// if the password is correct
-		if(password == alltoken[i].password) {
-			// send the token to the frontend
-			res.sendStatus(200, `Password: ${password} is correct!`)
-		}
-		// if the password is incorrect send alert message to the frontend
-		else {
-			// send alert
-			res.send((alert(`Password: ${password} is incorrect!`)))
-		}
+
+app.post('/upload', upload.single('file'), (req, res) => {
+	console.log('/upload-route triggered!')
+	// Check if the request includes a file
+	if (!req.file) {
+	  return res.status(400).send('No files were uploaded.')
 	}
-})
+  
+	// Get the file from the request
+	const file = req.file
+	const fileName = req.file.name
+	const author = req.body.author
+console.log(`file: `, file, `fileName: `, fileName, `author: `, author)
+	// if the author folder does not exist, create it
+	if (!fs.existsSync(`./backend/${author}`) && author != "") {
+		console.log(`./backend/${author}`)
+		fs.mkdirSync(`./backend/${author}`)
+	}
+	
+	// move the file to the author folder
+	fs.mv(file.path, `./backend/${author}`, (err) => {
+	  if (err) {
+		return res.status(500).send(err)
+	  }
+  
+	  // Return a success response
+	  return res.send(`File ${fileName} uploaded!`)
+	})
+  })
+  
 console.log('Starting the server')
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
+
+/*app.post('/authors', (req, res) => {
+	const author = {authorName: req.body.authorName, password: req.body.password}
+		// check if json-file not exists
+		if (!fs.existsSync('./p.json')) {
+			// create new file if not exists
+			fs.closeSync(fs.openSync('./p.json', 'w'))
+		}
+			//check if alltoken file is empty
+			if (alltoken.praxis.length == 0) {
+				// if empty, set id to 1
+				author.id = 1
+			}
+			else {
+				// if not empty, set id to the last id + 1
+				author.id = alltoken.praxis[alltoken.praxis.length - 1].id + 1
+			}
+	// add to p.json
+	alltoken.praxis.push(author)
+	fs.writeFileSync('./p.json', JSON.stringify(alltoken))
+	res.send('Author added!')
+
+	const token = req.headers['token']
+	if (!token) return res.sendStatus(400)
+	jwt.verify(token, secretValueFromENV, (err, decoded) => {
+		if (err) {
+			console.log(`Error: `, err)
+			return res.sendStatus(403)
+		}
+		console.log(`decoded: `, decoded)
+		return res.send(decoded)
+	})
+
+})*/
--- readme.md
+++ readme.md
@@ -2,14 +2,44 @@
 ## Tuesday, 13th:
 - [X] add a new comment for post
 - [X] local storage: author name
-- [ ] research for jwt / .md syntax
-- [ ] create a login screen
-## Thursday, 15th:
-- [ ] check the provided token and save it to local storage
-- [ ] if token is right, redirect to main screen
-- [ ] write a middleware to be able to just write a post when logged in. jwt-token-name to check
-	  author.name
+- [X] research for jwt / .md syntax
+- [X] create a login screen
+## Tuesday, 20th:
+- [X] check the provided token and save it to local storage
+- [X] if token is right, redirect to main screen
+- [ ] write a middleware to be able to just write a post when logged in. jwt-token-name to check author.name
 ## without date:
 - [ ] payload images or other type of files maybe
 - [ ] Mongo
 - [ ] Data analyzation from our sources
+- [ ] p.json data: get the token and console.log it("Praxis": praxisName, "token": token)
+
+# Blog
+## User can do
+- see the posts
+- see one post and comments to it
+- add a new comment to a post anonimously
+- (s) add a new comment to a post with author name
+- (s) add a new post
+- login with a token issued by us
+
+## Screens
+- login screen (optional for users)
+- post list
+- post detail
+- new post
+
+## Endpoints
+### Login screen
+- POST /login. Headers: {Auth: Token}
+- GET /login/verify. Headers: {Auth: Token}
+### Post list
+- GET /posts
+### Post detail
+- GET /posts/:id
+- GET /comments?postId=:id
+- POST /comment. Body: {postId: id, text: text}. Headers: {Auth: Token} (optional)
+### New post
+- POST /posts. Body: {title: title, text: text}. Headers: {Auth: Token}
+
+
